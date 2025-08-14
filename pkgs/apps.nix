d92d9476f8793c79183f503118933cf208b6c933
celt <mahstaru@gmail.com>
2025-08-14 14:54:55 +0200

    celt@steamhappy : Thu Aug 14 02:54:55 PM CEST 2025 -

--- pkgs/apps.nix
+++ pkgs/apps.nix
@@ -1,10 +1,4 @@
 {pkgs, ...}: {
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
-
   programs.firefox.enable = true;
   environment.systemPackages = with pkgs; [
     vlc
@@ -20,7 +14,5 @@
     vulkan-loader
     vdhcoapp
     spotify
-    nur.repos.nltch.spotify-adblock
-    nur.repos.nltch.ciscoPacketTracer8
   ];
 }
