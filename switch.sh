081637148cde0ac3466c66147b84646b6d00f866
c31t <mahstaru@gmail.com>
2025-08-21 19:45:36 +0200

    celt@steamhappy : Thu Aug 21 07:45:36 PM CEST 2025 -

--- switch.sh
+++ switch.sh
@@ -4,7 +4,7 @@ cd ~/nixconfig
 WHERE=$(cat /etc/hostname)
 alejandra .
 git add .
-git commit --allow-empty -m "$(whoami)@${WHERE} : $(date) - $@"
 sudo nixos-rebuild switch --show-trace --builders "" --flake .#$WHERE
+git commit --allow-empty -m "$(whoami)@${WHERE} : $(date) - $@"
 nix-env --delete-generations 7d
 nix-store --gc
