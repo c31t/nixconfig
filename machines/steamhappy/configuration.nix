577c374832c5abfe7ecb02be701118e5c8ce8cb9
c31t <mahstaru@gmail.com>
2025-08-21 22:43:36 +0200

    celt@steamhappy : Thu Aug 21 10:43:36 PM CEST 2025 -

--- machines/steamhappy/configuration.nix
+++ machines/steamhappy/configuration.nix
@@ -6,7 +6,7 @@
   pkgs,
   ...
 }: {
-  networking.hostName = "steamhappy"; # Define your hostname.
+  networking.hostName = "superduperincrediblelaptop"; # Define your hostname.
   # Set your time zone.
   time.timeZone = "Europe/Paris";
 
